2cf092bf5afd0611079f5200a74b2411a13d10d4
eleith <online+git@eleith.com>
2017-11-03 12:27:59 -0700

    updated logging

--- lambda-functions/jira-bot-filter-task-runner/index.js
+++ lambda-functions/jira-bot-filter-task-runner/index.js
@@ -16,19 +16,23 @@ const done = (callback) => {
 
 runTask = (Jira, startAt = 0) => {
   return task => {
-    Jira.search.search(
-      {
+
+    const jiraOptions = {
         jql: task.filter,
         fields: task.fields || ['id', 'key', 'comment', 'priority', 'created'],
-        expand: task.expand || ['changelog'],
         maxResults: 100, // max is 100
         startAt: startAt
-      },
-      (err, results) => {
+    };
+
+    if (task.expand) {
+      jiraOptions.expand = task.expand;
+    }
+
+    Jira.search.search(jiraOptions, (err, results) => {
         if (err) {
           console.error('filter failed: %s\n', task.filter, err);
         } else if (results && results.issues && results.issues.length) {
-          console.log(`${results.issues.length} issues returned for [ ${task.filter} ]`);
+          console.log(`${results.issues.length} issues starting at ${startAt} returned for [ ${task.filter} ]`);
           const filteredIssues = results.issues.filter(
             ConditionChecker.checkConditions(task.conditions || [])
           );
@@ -42,7 +46,6 @@ runTask = (Jira, startAt = 0) => {
           }
 
           if (results.issues && results.total > results.issues.length + startAt) {
-            console.log('running filter again to get more results');
             runTask(Jira, results.issues.length + startAt)(task);
           }
         } else {
